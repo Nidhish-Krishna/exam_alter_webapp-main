cddf64a9881bd8bc97035d23173384e6ab0c7ef8
Nidhish <65399661+Nidhish-Krishna@users.noreply.github.com>
2021-11-09 00:48:55 +0530

    Add files via upload

---
+++ scripts/before_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+
+
+#creating directory
+DIR="/home/ec2-user/exam_alter_webapp"
+if [-d "$DIR"]; then
+    echo "${DIR} exists"
+
+else 
+    echo "creating dir"
+    mkdir ${DIR}
+
+fi
